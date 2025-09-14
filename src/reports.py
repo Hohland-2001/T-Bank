f749497f8d3ed6ffa40c975cb1b1376c1391fb1e
Max Yanchuk <maksim.yanchuk.01@mail.ru>
2025-09-14 18:30:10 +0300

    Удалена папка .idea

---
+++ src/reports.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+def spending_by_category(transactions: pd.DataFrame,
+                         category: str,
+                         date: str = None) -> pd.DataFrame:
+    pass
